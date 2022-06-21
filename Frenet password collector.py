adbd6e6b2460e635b247fc6410d6ad3e373917cf
Andrea Virgillito <71944456+PythonUser-ux@users.noreply.github.com>
2022-06-21 14:27:13 +0200

    Update Frenet password collector.py

--- Frenet password collector.py
+++ Frenet password collector.py
@@ -7,14 +7,19 @@ import argparse
 parser = argparse.ArgumentParser(conflict_handler="resolve",add_help=False)
 parser.add_argument("-r","--reset",default=0, help=argparse.SUPPRESS)
 parser.add_argument("-ck","--change_key",default=0, help=argparse.SUPPRESS)
-parser.add_argument("-t","--timeout",default=7, help=argparse.SUPPRESS)
+parser.add_argument("-t","--timeout",default=15, help=argparse.SUPPRESS)
+parser.add_argument("-pt","--ptimeout",default=5, help=argparse.SUPPRESS)
+parser.add_argument("-vp","--value_path",default="Value", help=argparse.SUPPRESS)
+parser.add_argument("-kp","--key_path",default="Key", help=argparse.SUPPRESS)
 args,_ = parser.parse_known_args()
 
 
 
-V_NAME="Value"
-K_NAME="Key" # "C://Users//Andrea//Desktop//Folder1//Folder2//Key"
+V_PATH=args.value_path
+K_PATH=args.key_path # "C://Users//Andrea//Desktop//Folder1//Folder2//Key"
 HELP_TIMEOUT=args.timeout
+PASS_TIMEOUT=args.ptimeout
+PASSWORD="super_password"
 
 # By default relative paths point to the .py directory
 current_directory=os.path.dirname(__file__)
@@ -25,14 +30,14 @@ def write_key():
     Generates a key and save it into a key.key file in the same directory of the .py
     """
     key = Fernet.generate_key()
-    with open(re.escape(K_NAME), "wb") as key_file:
+    with open(re.escape(K_PATH), "wb") as key_file:
         key_file.write(key)
 
 def load_key():
     """
     Loads the key from the current directory named `key.key`
     """
-    return open(re.escape(K_NAME), "rb").read()
+    return open(re.escape(K_PATH), "rb").read()
 
 def encrypt(filename, key):
     """
@@ -66,18 +71,18 @@ def decrypt(filename, key):
 
 if args.reset:
    write_key()
-   A={"WEBSITE1":"password"} # initial key-password dictionary
-   with open(re.escape(V_NAME), "wb") as file_to_store:
+   A={"WEBSITE1":"password1"} # initial key-password dictionary
+   with open(re.escape(V_PATH), "wb") as file_to_store:
       pickle.dump(A, file_to_store)
    key = load_key()
-   encrypt(re.escape(V_NAME), key)
+   encrypt(re.escape(V_PATH), key)
 
 if args.change_key:
    key = load_key()
-   decrypt(re.escape(V_NAME), key)
+   decrypt(re.escape(V_PATH), key)
    write_key()
    key = load_key()
-   encrypt(re.escape(V_NAME), key)
+   encrypt(re.escape(V_PATH), key)
 
 
 clear = lambda: os.system('cls')
@@ -86,14 +91,14 @@ key = load_key()
 
 while True:
    password = input("Addendum 1: ")
-   if password=="lp":
+   if password==PASSWORD:
       clear()
       try:
-         decrypt(re.escape(V_NAME), key)
-         file_to_read = open(re.escape(V_NAME), "rb")
+         decrypt(re.escape(V_PATH), key)
+         file_to_read = open(re.escape(V_PATH), "rb")
          A = pickle.load(file_to_read)
          file_to_read.close()
-         encrypt(re.escape(V_NAME), key)
+         encrypt(re.escape(V_PATH), key)
       except Exception as e:
          print(e)
       #clear()
@@ -103,7 +108,7 @@ while True:
       if call not in ["ADD","REM","HELP"]:
          if call.upper() in A:
             print(A[call.upper()])
-            time.sleep(4.4)
+            time.sleep(PASS_TIMEOUT)
             clear()
          else:
             print("Not Found")
@@ -121,13 +126,17 @@ while True:
          clear()
       elif call=="ADD":    # to add or change a password
          call1=str(input()).upper()
-         call2=str(input())
-         A[call1]=call2
-         clear()
-         file_to_store = open(re.escape(V_NAME), "wb")
-         pickle.dump(A, file_to_store)
-         file_to_store.close()
-         encrypt(re.escape(V_NAME), key)
+         if call1 in ["ADD","REM","HELP"]:
+            print("It is not possible to use keywords as key")
+            time.sleep(0.6)
+         else:
+            call2=str(input())
+            A[call1]=call2
+            clear()
+            file_to_store = open(re.escape(V_PATH), "wb")
+            pickle.dump(A, file_to_store)
+            file_to_store.close()
+            encrypt(re.escape(V_PATH), key)
       elif call=="REM": # to remove a password
          call=str(input()).upper()
          try:
@@ -136,10 +145,10 @@ while True:
             print("Not Found")
             time.sleep(0.5)
          clear()
-         file_to_store = open(re.escape(V_NAME), "wb")
+         file_to_store = open(re.escape(V_PATH), "wb")
          pickle.dump(A, file_to_store)
          file_to_store.close()
-         encrypt(re.escape(V_NAME), key)
+         encrypt(re.escape(V_PATH), key)
    else:
       addendum2=input("Addendum 2: ")
       try:
